be0359b03655ef11eb0af5129ac9047127136771
Nongyao <nongyao.so@gmail.com>
2024-09-24 12:20:52 +0700

    update 24-9-67

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const routes = [
   {
     path: '/',
@@ -14,7 +15,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
+
 ]
 
 const router = createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Login">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mx-auto text-start bg-primary-subtle rounded">
+           
+      <h1 class="mt-4">Welcome to Login:</h1>
+      <form>
+        <label>Username:</label>
+        <input type="text" class="form-control" id="username"> <br>
+        <label>Password:</label>
+        <input type="password" id="password" class="form-control" >
+        <div class="text-center mb-4 mt-3 ">
+        <input type="button" id="submit" value="Login" class="btn btn-primary ">
+         </div>
+      </form>
+    </div>
+</div>
+    </div>
+    </div>
+  </template>
